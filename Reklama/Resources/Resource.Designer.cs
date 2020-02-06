1e824745e62dfd5dea487d799f86d06589c67064
YusupovZA <jusupovz@gmail.com>
2020-02-06 23:33:07 +0300

    add lang partial view

--- Reklama/Resources/Resource.Designer.cs
+++ Reklama/Resources/Resource.Designer.cs
@@ -61,29 +61,236 @@ namespace Resources {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Страна.
+        ///   Looks up a localized string similar to Найти.
         /// </summary>
-        public static string Country {
+        public static string BtnFind {
             get {
-                return ResourceManager.GetString("Country", resourceCulture);
+                return ResourceManager.GetString("BtnFind", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Заголовок.
+        ///   Looks up a localized string similar to Сбросить.
         /// </summary>
-        public static string IndexHeader {
+        public static string BtnReset {
             get {
-                return ResourceManager.GetString("IndexHeader", resourceCulture);
+                return ResourceManager.GetString("BtnReset", resourceCulture);
             }
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Имя.
+        ///   Looks up a localized string similar to Город.
         /// </summary>
-        public static string Name {
+        public static string FormCity {
             get {
-                return ResourceManager.GetString("Name", resourceCulture);
+                return ResourceManager.GetString("FormCity", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Выберите город.
+        /// </summary>
+        public static string FormCityPlacceholder {
+            get {
+                return ResourceManager.GetString("FormCityPlacceholder", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Поиск по тексту объявления.
+        /// </summary>
+        public static string FormFindByText {
+            get {
+                return ResourceManager.GetString("FormFindByText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Кол-во комнат.
+        /// </summary>
+        public static string FormFlatsCount {
+            get {
+                return ResourceManager.GetString("FormFlatsCount", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Этажей не ниже.
+        /// </summary>
+        public static string FormFloorFrom {
+            get {
+                return ResourceManager.GetString("FormFloorFrom", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Этажей не выше.
+        /// </summary>
+        public static string FormFloorTo {
+            get {
+                return ResourceManager.GetString("FormFloorTo", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Упорядочить по.
+        /// </summary>
+        public static string FormSortDirection {
+            get {
+                return ResourceManager.GetString("FormSortDirection", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Возрастанию.
+        /// </summary>
+        public static string FormSortDirectionAsc {
+            get {
+                return ResourceManager.GetString("FormSortDirectionAsc", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Убыванию.
+        /// </summary>
+        public static string FormSortDirectionDesc {
+            get {
+                return ResourceManager.GetString("FormSortDirectionDesc", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Сортировать по.
+        /// </summary>
+        public static string FormSortField {
+            get {
+                return ResourceManager.GetString("FormSortField", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Дата.
+        /// </summary>
+        public static string FormSortFieldDate {
+            get {
+                return ResourceManager.GetString("FormSortFieldDate", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Цена.
+        /// </summary>
+        public static string FormSortFieldPrice {
+            get {
+                return ResourceManager.GetString("FormSortFieldPrice", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Цена за м&lt;sup&gt;2&lt;/sup&gt;.
+        /// </summary>
+        public static string FormSortFieldPriceForSquare {
+            get {
+                return ResourceManager.GetString("FormSortFieldPriceForSquare", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Количеству м&lt;sup&gt;2&lt;/sup&gt;.
+        /// </summary>
+        public static string FormSortFieldSquare {
+            get {
+                return ResourceManager.GetString("FormSortFieldSquare", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Площадь от м&lt;sup&gt;2&lt;/sup&gt;.
+        /// </summary>
+        public static string FormSquareFrom {
+            get {
+                return ResourceManager.GetString("FormSquareFrom", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Площадь до м&lt;sup&gt;2&lt;/sup&gt;.
+        /// </summary>
+        public static string FormSquareTo {
+            get {
+                return ResourceManager.GetString("FormSquareTo", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Разместить объявление.
+        /// </summary>
+        public static string MenuAddAd {
+            get {
+                return ResourceManager.GetString("MenuAddAd", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Соглашение.
+        /// </summary>
+        public static string MenuContract {
+            get {
+                return ResourceManager.GetString("MenuContract", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Личный кабинет.
+        /// </summary>
+        public static string MenuDashboard {
+            get {
+                return ResourceManager.GetString("MenuDashboard", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Фильтры.
+        /// </summary>
+        public static string MenuFilters {
+            get {
+                return ResourceManager.GetString("MenuFilters", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Главная.
+        /// </summary>
+        public static string MenuMain {
+            get {
+                return ResourceManager.GetString("MenuMain", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Контакты.
+        /// </summary>
+        public static string RealtyContacts {
+            get {
+                return ResourceManager.GetString("RealtyContacts", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to ком. кв..
+        /// </summary>
+        public static string RealtyFlatShort {
+            get {
+                return ResourceManager.GetString("RealtyFlatShort", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to эт..
+        /// </summary>
+        public static string RealtyFloorShort {
+            get {
+                return ResourceManager.GetString("RealtyFloorShort", resourceCulture);
             }
         }
     }
